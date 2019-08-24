b32adc660f6d481c16f0f8e785b8a44bf7cb50f4
Emrah Ayanoglu <emrahayanoglu@gmail.com>
2019-08-24 16:47:03 +0200

    removed build

--- Build/third-party/nlohmann-json/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/emrahayanoglu/cpp_workspace/sample-mqtt-project/third-party/nlohmann-json
-# Build directory: /Users/emrahayanoglu/cpp_workspace/sample-mqtt-project/Build/third-party/nlohmann-json
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
