aa2deb9748f3099869db9edebc4d456dd7fc1a25
Emrah Ayanoglu <emrahayanoglu@gmail.com>
2019-08-24 17:21:49 +0200

    Removed submodule

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
 #include "hello.h"
 #include <iostream>
+#include <string>
 
 #include <nlohmann/json.hpp>
 #include "spdlog/spdlog.h"
+#include "yaml-cpp/yaml.h"
+#include "mqtt/async_client.h"
 
 // for convenience
 using json = nlohmann::json;
@@ -20,4 +23,10 @@ int main()
     std::cout << "Pi: " << j.dump() << std::endl;
 
     spdlog::info("Welcome to spdlog!");
+
+    YAML::Node config = YAML::LoadFile("/Users/emrahayanoglu/cpp_workspace/sample-mqtt-project/hello.yaml");
+
+    if (config["environment"]) {
+        std::cout << "Last logged in: " << config["environment"].as<std::string>() << "\n";
+    }
 }
